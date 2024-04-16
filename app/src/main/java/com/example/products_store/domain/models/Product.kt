28e157d3fe22fed7c86f2046cde0cd569de5596e
Dorin Dorsman <dorin.dorsman@intel.com>
2024-04-16 22:41:46 +0300

    add shop product

--- app/src/main/java/com/example/products_store/domain/models/Product.kt
+++ app/src/main/java/com/example/products_store/domain/models/Product.kt
@@ -2,60 +2,6 @@ package com.example.products_store.domain.models
 
 import com.google.gson.annotations.SerializedName
 
-//data class Product(
-//    @SerializedName("id")
-//    var id: Int,
-//
-//    @SerializedName("title")
-//    var title: String,
-//
-//    @SerializedName("description")
-//    var description: String,
-//
-//    @SerializedName("price")
-//    var price: Int,
-//
-//    @SerializedName("discountPercentage")
-//    var discountPercentage: Double,
-//
-//    @SerializedName("rating")
-//    var rating: Double,
-//
-//    @SerializedName("stock")
-//    var stock: String,
-//
-//    @SerializedName("brand")
-//    var brand: String,
-//
-//    @SerializedName("category")
-//    var category: String,
-//
-//    @SerializedName("thumbnail")
-//    var thumbnail: String,
-//
-//    @SerializedName("images")
-//    var images: List<String>,
-//
-//    @SerializedName("favorite")
-//    var favorite: Boolean = false
-//){
-//    companion object{
-//        const val SEPARATOR = "|"
-//    }
-//
-//    fun convertImagesToString(): String {
-//        val sb: StringBuilder = StringBuilder()
-//        var firstInList = true
-//        for (image in images) {
-//            if (!firstInList) sb.append(SEPARATOR)
-//            sb.append(image)
-//            firstInList = false
-//        }
-//        return sb.toString()
-//    }
-//}
-
-
 data class Product(
     @SerializedName("id")
     var id: Int? = null,
--- app/src/main/java/com/example/products_store/domain/models/Shop.kt
+++ app/src/main/java/com/example/products_store/domain/models/Shop.kt
@@ -2,20 +2,6 @@ package com.example.products_store.domain.models
 
 import com.google.gson.annotations.SerializedName
 
-//data class Shop(
-//    @SerializedName("products")
-//    var products: List<Product>? = null,
-//
-//    @SerializedName("total")
-//    var total: Int? = null,
-//
-//    @SerializedName("skip")
-//    var skip: Int? = null,
-//
-//    @SerializedName("limit")
-//    var limit: Int? = null,
-//)
-
 data class Shop(
     @SerializedName("products")
     var products: List<Product> = emptyList(),
